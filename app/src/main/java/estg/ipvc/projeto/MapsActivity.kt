72ed4fb130384aeb38ee7ed6d72ec396857f4fab
joantunes <joantunes@ipvc.pt>
2020-12-02 22:11:26 +0000

    Login feito, Maps too

--- app/src/main/java/estg/ipvc/projeto/MapsActivity.kt
+++ app/src/main/java/estg/ipvc/projeto/MapsActivity.kt
@@ -1,10 +1,9 @@
 package estg.ipvc.projeto
 
-import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -19,63 +18,45 @@ import retrofit2.Response
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var users: List<user>
-    private lateinit var Problems: List<problems>
-    private lateinit var lastLocation: Location
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
-    
+    private lateinit var problems: List<problems>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
-        ///////////CALL MAPS
+///////////////////////////// call the service and add markers ///////////////////////////////////////////////////
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getProblems()
         var position: LatLng
+
         call.enqueue(object : Callback<List<problems>> {
-                override fun onResponse(call: Call<List<problems>>, response: Response<List<problems>>) {
-
-                if (response.isSuccessful) {
-                    Problems = response.body()!!
-                    for (problems in Problems) {
-                        position = LatLng(problems.lat.toDouble(), problems.lng.toDouble())
-                        mMap.addMarker(MarkerOptions().position(position).title(problems.descr + " - " + problems.city))
-
+            override fun onResponse(call: Call<List<problems>>, response: Response<List<problems>>) {
+                if (response.isSuccessful) {     // Em caso de sucesso
+                    problems = response.body()!! // igualar a lista de problemas
+                    for (problem in problems) {
+                        position = LatLng(problem.lat.toDouble(), //latlng precisa de recerber um double da longitude e latitude
+                                problem.lng.toDouble())
+                        mMap.addMarker(MarkerOptions().position(position).title(problem.descr + " - " + problem.city))
 
                     }
                 }
             }
 
             override fun onFailure(call: Call<List<problems>>, t: Throwable) {
-                Toast.makeText(this@MapsActivity,"${t.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MapsActivity, "${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-      /*  val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        */
 
     }
 }
-
